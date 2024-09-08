3f550d4c713063f639080c463ebeca4b45d9eb13
chwello <fretchelm.gerarman@gmail.com>
2024-09-08 13:14:06 +0800

    remove duplicates in zod schema

--- src/schema/signin.ts
+++ src/schema/signin.ts
@@ -7,11 +7,9 @@ export const SigninFormSchema = z
   .object({
     email: z
       .string()
-      .email("Email is required")
       .email("Incorrect email"),
     password: z
       .string()
       .min(6, "Password must be at least 6 characters")
       .max(50, "Password must be less than 50 characters")
-      .max(50, "Incorrect password"),
   });
