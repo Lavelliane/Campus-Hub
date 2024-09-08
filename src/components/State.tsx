1559c0c487d7c5a3bfc2bc6d267a4a137be999d6
Jhury Kevin <jhurylastre432@gmail.com>
2024-09-08 21:28:26 +0800

    feature: create demo files

---
+++ src/components/State.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React, { useState } from 'react'
+
+function State() {
+    const [state, setState] = useState(0)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default State
